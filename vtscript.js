5856bcb6a1c20fd90955e9c242a8caf1ac65de53
ayobami_ogundiran <ayobami_ogundiran@yahoo.com>
2022-09-16 00:14:50 -0700

    add viewtern

---
+++ vtscript.js
@@ -0,0 +1,565 @@
+
+const Questions = [
+    {
+        question: "How many columns does this website have?",
+        image: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/facebook.png',
+        correctionImage: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/facebook_correction.png', 
+        options: {
+            a: 5,
+            b: 3,
+            c: 4,
+            d: 1
+        },
+        answer: "b"
+    },
+    {
+        question: "How manycolumns does this conent area of this website have?",
+        image: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/nytimes.png',
+        correctionImage: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/nytimes_2_columns_correction.png', 
+        options: {
+            a: 2,
+            b: 4,
+            c: 3,
+            d: 7
+        },
+        answer: "a"
+    },
+    {
+        question: "How many rows does this website have?",
+        image: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/nairaland.png',
+        correctionImage: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/nairaland_vertical_division_correction.png', 
+        options: {
+            a: 3,
+            b: 6,
+            c: 4,
+            d: 2,
+        },
+        answer: "c"
+    },
+    {
+        question: "How many rows and columns does this website have?",
+        image: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/edge.png',
+        correctionImage: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/edge_correction.png', 
+        options: {
+            a: 2,
+            b: 1,
+            c: 5,
+            d: 3
+        },
+        answer: "d"
+    },   
+    {
+        question: "How many columns does this website have?",
+        image: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/twitter.png',
+        correctionImage: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/twitter_correction.png', 
+        options: {
+            a: 9,
+            b: 1,
+            c: 3,
+            d: 4
+        },
+        answer: "c"
+    },
+    {
+        question: "How many columns does this website have?",
+        image: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/chrome-page.png',
+        correctionImage: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/chrome-page_correction_1_division.png', 
+        options: {
+            a: 1,
+            b: 3,
+            c: 6,
+            d: 4
+        },
+        answer: "a"
+    },
+    {
+        question: "How many vertical divisions does this website have?",
+        image: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/akana.png',
+        correctionImage: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/akana_correction.png', 
+        options: {
+            a: 2,
+            b: 5,
+            c: 3,
+            d: 4
+        },
+        answer: "b"
+    },
+    {
+        question: "How many columns does this website have?",
+        image: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/airship.png',
+        correctionImage: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/airship_correction.png', 
+        options: {
+            a: 2,
+            b: 5,
+            c: 3,
+            d: 1
+        },
+        answer: "d"
+    },
+    {
+        question: "How many columns does this website have?",
+        image: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/fb-login.png',
+        correctionImage: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/fb-login_correction.png', 
+        options: {
+            a: 3,
+            b: 5,
+            c: 2,
+            d: 4
+        },
+        answer: "c"
+    },
+    {
+        question: "How many columns does this website have?",
+        image: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/tibco.png',
+        correctionImage: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/tibco_correction.png', 
+        options: {
+            a: 3,
+            b: 2,
+            c: 5,
+            d: 4
+        },
+        answer: "b"
+    },
+    {
+        question: "How many columns does this website have?",
+        image: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/twitter-login.png',
+        correctionImage: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/twitter-login_correction.png', 
+        options: {
+            a: 4,
+            b: 3,
+            c: 2,
+            d: 5
+        },
+        answer: "c"
+    },
+    {
+        question: "How many columns does this website have?",
+        image: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/atatus.png',
+        correctionImage: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/atatus_correction.png', 
+        options: {
+            a: 1,
+            b: 6,
+            c: 3,
+            d: 2
+        },
+        answer: "d"
+    },
+    {
+        question: "How many columns does this website have?",
+        image: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/google-result.png',
+        correctionImage: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/google-result_correction.png', 
+        options: {
+            a: 4,
+            b: 2,
+            c: 3,
+            d: 5
+        },
+        answer: "b"
+    },
+    {
+        question: "How many columns does this website have?",
+        image: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/hashnode.png',
+        correctionImage: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/hashnode_correction', 
+        options: {
+            a: 1,
+            b: 4,
+            c: 5,
+            d: 2
+        },
+        answer: "a"
+    },
+    {
+        question: "How many columns does this website have?",
+        image: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/rapidAPI.png',
+        correctionImage: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/rapidAPI_correction', 
+        options: {
+            a: 4,
+            b: 3,
+            c: 1,
+            d: 2
+        },
+        answer: "d"
+    },
+    {
+        question: "How many columns does this website have?",
+        image: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/moesif.png',
+        correctionImage: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/moesif_correction.png', 
+        options: {
+            a: 5,
+            b: 3,
+            c: 1,
+            d: 2
+        },
+        answer: "b"
+    },
+    {
+        question: "How many columns does this website have?",
+        image: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/codepen.png',
+        correctionImage: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/codepen_correction.png', 
+        options: {
+            a: 5,
+            b: 3,
+            c: 1,
+            d: 2
+        },
+        answer: "d"
+    },
+    {
+        question: "How many columns does this component have?",
+        image: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/grid-cards.png',
+        correctionImage: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/grid-cards_correction.png', 
+        options: {
+            a: 5,
+            b: 4,
+            c: "grid 3 columns",
+            d: 2
+        },
+        answer: "c"
+    },
+    {
+        question: "How many columns does this component have?",
+        image: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/3-columns-component.png',
+        correctionImage: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/3-columns-component_correction.png', 
+        options: {
+            a: 4,
+            b: 2,
+            c: 1,
+            d: 3
+        },
+        answer: "d"
+    },
+    {
+        question: "How many columns does this component have?",
+        image: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/yahoo-inbox-ms.png',
+        correctionImage: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/yahoo-inbox-ms_correction.png', 
+        options: {
+            a: 5,
+            b: 4,
+            c: 3,
+            d: 1
+        },
+        answer: "c"
+    },
+    {
+        question: "How many columns does comment box have?",
+        image: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/fb-comment-box.png',
+        correctionImage: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/fb-comment-box_correction.png', 
+        options: {
+            a: 2,
+            b: 4,
+            c: 5,
+            d: 3
+        },
+        answer: "a"
+    },
+    {
+        question: "How many columns does this component have?",
+        image: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/github-commit.png',
+        correctionImage: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/github-commit_correction.png', 
+        options: {
+            a: 3,
+            b: 6,
+            c: 4,
+            d: 1
+        },
+        answer: "a"
+    },
+    {
+        question: "How many columns does this component have?",
+        image: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/two-columns-profile.png',
+        correctionImage: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/two-columns-profile_correction.png', 
+        options: {
+            a: 1,
+            b: 7,
+            c: 2,
+            d: 4
+        },
+        answer: "c"
+    },
+    {
+        question: "How many columns does this component have?",
+        image: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/3-columns-component-2.png',
+        correctionImage: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/3-columns-component-2_correction.png', 
+        options: {
+            a: 5,
+            b: 4,
+            c: 2,
+            d: 3
+        },
+        answer: "d"
+    },
+    {
+        question: "How many columns does comment box have?",
+        image: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/3-columns-header-2.png',
+        correctionImage: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/3-columns-header-2_correction.png', 
+        options: {
+            a: 5,
+            b: 4,
+            c: 2,
+            d: 3
+        },
+        answer: "d"
+    },
+    {
+        question: "How many columns does this component have?",
+        image: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/3-columns-footer.png',
+        correctionImage: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/3-columns-footer_correction.png', 
+        options: {
+            a: 5,
+            b: 3,
+            c: 4,
+            d: 2
+        },
+        answer: "b"
+    },
+    {
+        question: "How many columns does this component have?",
+        image: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/2-columns-component-1.png',
+        correctionImage: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/2-columns-component-1_correction.png', 
+        options: {
+            a: 4,
+            b: 2,
+            c: 5,
+            d: 1
+        },
+        answer: "b"
+    },
+    {
+        question: "How many columns does this component have?",
+        image: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/two-column-feature-info.png',
+        correctionImage: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/two-column-feature-info_correction.png', 
+        options: {
+            a: 5,
+            b: 3,
+            c: 2,
+            d: 1
+        },
+        answer: "c"
+    },
+    {
+        question: "How many columns does this component have?",
+        image: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/grid-features.png',
+        correctionImage: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/grid-features_correction.png', 
+        options: {
+            a: 4,
+            b: 0,
+            c: 2,
+            d: "grid 2 columns"
+        },
+        answer: "d"
+    },
+    {
+        question: "How many columns does this component have?",
+        image: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/fb-post.png',
+        correctionImage: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/fb-post_correction.png', 
+        options: {
+            a: 2,
+            b: 4,
+            c: "grid 3 columns",
+            d: "2 columns & 3 grid columns"
+        },
+        answer: "d"
+    },
+    {
+        question: "How many columns does this component have?",
+        image: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/header-bar-1.png',
+        correctionImage: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/header-bar-1_correction.png', 
+        options: {
+            a: 1,
+            b: 4,
+            c: 3,
+            d: 2
+        },
+        answer: "c"
+    },
+    {
+        question: "How many columns does this component have?",
+        image: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/one-column-card.png',
+        correctionImage: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/one-column-card_correction.png', 
+        options: {
+            a: 2,
+            b: 4,
+            c: 5,
+            d: 1
+        },
+        answer: "d"
+    },
+    {
+        question: "How many columns does this section have?",
+        image: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/six-columns-component.png',
+        correctionImage: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/six-columns-component_correction.png', 
+        options: {
+            a: 2,
+            b: 7,
+            c: 6,
+            d: 4
+        },
+        answer: "d"
+    },
+    {
+        question: "How many columns does this component have?",
+        image: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/fb-comment.png',
+        correctionImage: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/fb-comment_correction.png', 
+        options: {
+            a: 1,
+            b: 3,
+            c: 2,
+            d: 4
+        },
+        answer: "c"
+    },
+    {
+        question: "How many columns does this component have?",
+        image: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/twitter_tweet.png',
+        correctionImage: 'https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/twitter_tweet_correction.png', 
+        options: {
+            a: 2,
+            b: 3,
+            c: 5,
+            d: 4
+        },
+        answer: "a"
+    }
+];
+
+let currentQuestionIndex = 0;
+const lastImageIndex = Questions.length - 1;
+
+const display = (currentQuestionIndex) => {
+    //display slide image
+    const currentQuestion = Questions[currentQuestionIndex];
+    const sliderImage = document.getElementById('slider_image');
+    sliderImage.src = currentQuestion.image;
+
+    makeOption(currentQuestion, currentQuestionIndex);
+   //create slide number
+    makeSlideNumber(currentQuestionIndex, lastImageIndex);
+}
+
+function makeSlideNumber (currentQuestionIndex, lastImageIndex) {
+    const slideNumber = `${currentQuestionIndex + 1} / ${lastImageIndex + 1}`;
+
+    const slideNumberUI = document.querySelector('.slide-number');
+    slideNumberUI.innerHTML = slideNumber;
+}
+
+const makeOption = (question, currentQuestionIndex) => {
+    const options = [];
+    const quiz = document.querySelector("#quiz")
+    console.log(quiz)
+    for(const letter in question.options) {
+        options.push(
+            `<li class="option">
+                <label id = "${letter}">
+                    <input type="radio" name="question${currentQuestionIndex}" id="${letter}" onclick="markQuestion(event)">
+                    <span id="${letter}" style="margin-left:10px"> 
+                        ${question.options[letter]}
+                    </span>
+                </label>
+            </li>`
+        );
+    }
+    quiz.innerHTML = `
+                    <div class="question">${question.question}</div>
+                      <ul class="options">
+                        ${options.join(" ")}
+                    </ul>`
+}
+
+display(currentQuestionIndex);// Display the first image in the array
+
+const markQuestion = (event) => {
+
+    const userAnswer = event.target.id;
+    const answer = Questions[currentQuestionIndex].answer;
+    
+    if( userAnswer != answer ) {
+        notify("Wrong answer", false);
+        return true;
+    }
+    notify("Correct answer", true)
+}
+
+const notify = (message, status) => {
+    const nextQuestionIndex = currentQuestionIndex + 1;
+    const quiz = document.querySelector("#quiz")
+    const icon = status ? "checkmark.png" : "cancel.png";
+    const button = status ? next(nextQuestionIndex) : watchTutorial(nextQuestionIndex);
+    const messageView = `
+    <div class="icon-container zoom-in-out-box">
+        <img src="https://res.cloudinary.com/nyscapp/image/upload/v1662907001/viewtern/${icon}">
+    </div>
+    <div class="error-message">${message}</div>
+    ${button}`; 
+    quiz.innerHTML = messageView;
+}
+
+const next = (index) => {
+    currentQuestionIndex = index;
+    if(currentQuestionIndex === Questions.length){
+        return `
+        <a href="https://youtoocancode.com/html-css"> Learn HTML & CSS</a>
+        `
+    }
+    return `<button onclick="display(${index})"> Next </button>
+    `
+}
+
+const watchTutorial = (index) => {
+    const correctionCordinate = {
+        x:''
+    }
+    
+    makeCorrection(Questions[currentQuestionIndex]);
+    return `${next(index)}`;
+}
+
+const makeCorrection = (question) => {
+    const sliderImage = document.getElementById('slider_image');
+    sliderImage.src = question.correctionImage;
+}
+
+const resolveElement = (obj) => {
+    if(obj.nodeType === 1) {
+        return obj;
+    } else if (obj.target) {
+        return obj.target;
+    }
+    throw("element and events are expected");
+}
+
+const hide = (element) => {
+    const tag = resolveElement(element);
+    tag.classList.add('hide');
+}
+
+const show = (element) => {
+    const tag = resolveElement(element);
+    tag.classList.remove('hide');
+}
+
+const showGame = () => {
+    const btn = document.querySelector('.btn');
+    btn.addEventListener('click', (event) => {
+        const landing = document.querySelector('.landing');
+        const game = document.querySelector('.game');
+        hide(landing);
+        show(game)
+    })
+}
+showGame();
+
+/* let questionId = 0;
+const options = document.querySelectorAll('.option');
+const option = options[questionId];
+
+option.addEventListener('click',(event) => {
+    const selector = `input[name=question${questionId}]:checked`;
+    const answer = (option.querySelector(selector) || {}).value;
+    console.log(event.target.value)
+})
+
+const answerContent = option.querySelector(`#${userAnswer}`);
+
+ */
+
